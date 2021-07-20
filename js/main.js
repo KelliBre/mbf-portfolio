5eacff6291cda0c8b9d9c882bff08d75972f3ee9
KelliBre <Kelli.Brecunier@gmail.com>
2021-07-20 13:51:35 +1000

    Updated mobile tablet and desktop views

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-const toggleBtn = document.querySelector('.navbar__toggleBtn');
-const menu = document.querySelector('.navbar__menu');
-const icons = document.querySelector('.navbar__icons');
+const toggleBtn = document.querySelector('.navbar-togglebtn');
+const menu = document.querySelector('.navbar-menu');
+const icons = document.querySelector('.navbar-icons');
 
 toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
